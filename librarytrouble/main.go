f8ebd8f7935d16af6f589a1a577828f6d926990a
Reza Hosseini <moha.rezahosseini@gmail.com>
2025-11-26 11:48:47 +0100

    add structure of librarytrouble

--- librarytrouble/main.go
+++ librarytrouble/main.go
@@ -1,16 +1,40 @@
 package library
 
+import (
+	"strings"
+)
+
+type Book struct {
+	title    string
+	borrowed bool
+	borrower string
+}
 type Library struct {
+	books    map[string]*Book
+	capacity int
 }
 
 func NewLibrary(capacity int) *Library {
-	// TODO
-	return nil
+	return &Library{
+		books:    make(map[string]*Book),
+		capacity: capacity,
+	}
 }
 
 func (library *Library) AddBook(name string) string {
-	// TODO
-	return ""
+	rawName := strings.TrimSpace(strings.ToLower(name))
+	if _, ok := library.books[rawName]; ok {
+		return "The book is already in the library"
+	}
+	if library.capacity <= len(library.books) {
+		return "Not enough capacity"
+	}
+	library.books[rawName] = &Book{
+		title:    strings.TrimSpace(name),
+		borrowed: false,
+		borrower: "",
+	}
+	return "OK"
 }
 
 func (library *Library) BorrowBook(bookName, personName string) string {
